b66ec2649a8b7db3c792274c34b5fb9d00bc8700
zzj2404 <233050490@qq.com>
2021-03-26 15:52:12 +0800

    '3.26,login'

--- app/src/main/java/com/example/androidftpclient/MainActivity.java
+++ app/src/main/java/com/example/androidftpclient/MainActivity.java
@@ -3,20 +3,32 @@ package com.example.androidftpclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button login;
 
+    private FTPClient ftpClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.UserName);
+        password = findViewById(R.id.Password);
+        login = findViewById(R.id.Login);
 
-        FTPClient ftpClient = new FTPClient();
+        ftpClient = new FTPClient();
 
         //根据服务器配置设置字符编码
         ftpClient.setControlEncoding("UTF-8");
@@ -24,30 +36,64 @@ public class MainActivity extends AppCompatActivity {
         //设置连接服务器超时时间
         ftpClient.setConnectTimeout(10000);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+
+
+
+
+
+
+//        try {
+//            //登录
+//            ftpClient.login(this.username.getText().toString(), this.password.getText().toString());
+//
+//            //获取当前文件列表
+//            FTPFile[] ftpFiles = ftpClient.listFiles();
+//
+//            //进入目录
+//            ftpClient.changeWorkingDirectory("directory1");
+//
+//            //返回上层目录
+//            ftpClient.changeToParentDirectory();
+//
+//            //新建文件夹
+//            ftpClient.makeDirectory("myDirectory");
+//
+//            //删除文件
+//            ftpClient.deleteFile("a.txt");
+//
+//            //重命名文件
+//            ftpClient.rename("旧文件名.txt","新文件名.txt");
+//            //aaaaaaaa
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    private void Login(){
         try {
             //登录
-            ftpClient.login("username", "password");
-
-            //获取当前文件列表
-            FTPFile[] ftpFiles = ftpClient.listFiles();
-
-            //进入目录
-            ftpClient.changeWorkingDirectory("directory1");
-
-            //返回上层目录
-            ftpClient.changeToParentDirectory();
-
-            //新建文件夹
-            ftpClient.makeDirectory("myDirectory");
-
-            //删除文件
-            ftpClient.deleteFile("a.txt");
-
-            //重命名文件
-            ftpClient.rename("旧文件名.txt","新文件名.txt");
-            //aaaaaaaa
+            ftpClient.connect("10.250.154.69",21);
+            ftpClient.login(this.username.getText().toString(), this.password.getText().toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
+        int replyCode = ftpClient.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(replyCode)) {
+            try {
+                ftpClient.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(MainActivity.this, "登录成功^_^", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(MainActivity.this, "登录失败>_<", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
